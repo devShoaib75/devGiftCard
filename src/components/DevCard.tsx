dbda9f107bf70a08235a64f427194f1f779169d2
devShoaib <shoaib628233@gmail.com>
2026-01-05 16:24:20 +0600

    img layout fix

--- src/components/DevCard.tsx
+++ src/components/DevCard.tsx
@@ -81,7 +81,7 @@ export const DevCard: React.FC<Props> = ({ user, repos }) => {
             <div className="qr-wrap">
               <QRCodeSVG 
                 value={shareUrl} 
-                size={90}
+                size={75}
                 bgColor="transparent"
                 fgColor="#7c5cff"
                 level="M"
